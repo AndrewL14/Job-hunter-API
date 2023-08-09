906613dc98e64918eefd0a9aee877d66b77745bd
Andrew Lam <andrew.x.li.lam@gmail.com>
2023-08-09 07:21:04 -0500

    convert from entity to DTO.

---
+++ src/main/java/jobs/search/JobHunter/converter/CustomConverter.java
@@ -0,0 +1,12 @@
+package jobs.search.JobHunter.converter;
+
+import jobs.search.JobHunter.DTO.JobPostDTO;
+import jobs.search.JobHunter.entity.JobPost;
+
+public class CustomConverter {
+
+    public static JobPostDTO jobToDto(JobPost post) {
+        return new JobPostDTO(post.getImbdId(), post.getProfile()
+                ,post.getDescription() , post.getExperience() , post.getSkills());
+    }
+}
